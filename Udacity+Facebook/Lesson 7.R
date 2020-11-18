677abe658ab8eaaf78c9fa322efec61bbe5435b6
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-11-18 01:52:39 -0800

    .

--- Udacity+Facebook/Lesson 7.R
+++ Udacity+Facebook/Lesson 7.R
@@ -41,7 +41,37 @@ ggplot(aes(x=age, y=female/male), data=pf.fc_by_age_gender.wide) +
 # Create a variable called year_joined
 # in the pf data frame using the variable
 pf$year_joined <- floor(2014-pf$tenure/365)
-  
-  
+# Using cut to bucket to custom(?) groups
+pf$year_joined.bucket <- cut(pf$year_joined, breaks = c(2004,2009,2011,2012,2014))
+table(year_joined.bucket)
 
+#Draw seperate line graph for each bucket years plus grand mean
+ggplot(aes(x = age, y = friend_count),
+       data = subset(pf, !is.na(gender), !is.na(year_joined))) +
+  geom_line(stat = 'summary', fun.y=mean, linetype= 2) +
+  geom_line(aes(color = year_joined.bucket), stat = 'summary', fun.y = mean)
 
+#Subset another dataframe to get friend_count/tenure to get friend rate
+with(subset(pf, tenure>=1), summary(friend_count/tenure))
+
+ggplot(aes(x = tenure, y = friendships_initiated/tenure),
+       data = subset(pf, tenure>= 1, !is.na(friendships_initiated))) +
+  geom_line(aes(color = year_joined.bucket), stat = 'summary', fun.y = mean)
+
+ggplot(aes(x = 7 * round(tenure / 7), y = friendships_initiated / tenure),
+       data = subset(pf, tenure > 0)) +
+  geom_smooth(aes(color = year_joined.bucket))
+
+#YOGURT DATASET
+yo <- read.csv('yogurt.csv')
+qplot(data=yo, x=price, binwidth=2)
+
+# Create a new variable called all.purchases,
+# which gives the total counts of yogurt for
+# each observation or household.
+yo <- transform(yo, all.purchases = strawberry+blueberry+pina.colada+plain+mixed.berry)
+qplot(data=yo, x=all.purchases, binwidth = 1)
+
+#Creat better vis
+ggplot(aes(x=time,y=price),data=yo)+
+  geom_jitter(alpha=1/4, shape=21)
